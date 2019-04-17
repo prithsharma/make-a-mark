119a8e52e3c5859481928025103cf958fa52e1f1
Prithvi Sharma <prithsharma100@gmail.com>
2019-04-17 22:30:22 +0530

    Set up eslint

---
+++ .eslintrc.js
@@ -0,0 +1,59 @@
+module.exports = {
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+    },
+  },
+  env: {
+    browser: true,
+    jest: true,
+    'react-native/react-native': true,
+  },
+  extends: [
+    'eslint:recommended',
+    'airbnb',
+    'plugin:react/recommended',
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ios.js', '.android.js'],
+      },
+    },
+  },
+  plugins: [
+    'react',
+    'react-native',
+  ],
+  rules: {
+    // overrides
+
+    'no-underscore-dangle': ['error', {
+      allowAfterThis: true,
+    }],
+
+    // disable this rule for require used in images etc.
+    'global-require': 0,
+
+    // `.jsx` extension cannot be used with React Native
+    // https://github.com/airbnb/javascript/issues/982
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+
+    // TODO - change this to default
+    // 'react/forbid-prop-types': ['error', { forbid: ['any'] }],
+    'padded-blocks': ['error', {
+      classes: 'never',
+      blocks: 'never',
+      switches: 'never',
+    }],
+
+    // rules from eslint-plugin-react-native
+    'react-native/no-unused-styles': ['error'],
+    'react-native/no-color-literals': ['error'],
+
+    'no-restricted-globals': ['error', 'navigator']
+  },
+};
+
--- App.js
+++ App.js
@@ -6,30 +6,30 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {
+  Platform, StyleSheet, Text, View,
+} from 'react-native';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu',
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    'Double tap R on your keyboard to reload,\n'
+    + 'Shake or press menu button for dev menu',
 });
+let styles;
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Welcome to React Native!</Text>
+      <Text style={styles.instructions}>To get started, edit App.js</Text>
+      <Text style={styles.instructions}>{instructions}</Text>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({
+styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
