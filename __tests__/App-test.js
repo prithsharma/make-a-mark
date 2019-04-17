972f6150f08c1deb0b68e34260652df6b9323dbd
Prithvi Sharma <prithsharma100@gmail.com>
2019-04-17 22:55:06 +0530

    Update sample test with react-native-testing-library

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -4,11 +4,14 @@
 
 import 'react-native';
 import React from 'react';
+import { shallow } from 'react-native-testing-library';
 import App from '../App';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+// import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  const comp = shallow(<App />);
+
+  expect(comp.output).toMatchSnapshot();
 });
