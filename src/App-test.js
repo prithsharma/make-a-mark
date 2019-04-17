08153734ece5d42cdf258efa689f664b3c0a0554
Prithvi Sharma <prithsharma100@gmail.com>
2019-04-17 22:59:00 +0530

    Modify directory structure

--- src/App-test.js
+++ src/App-test.js
@@ -5,7 +5,7 @@
 import 'react-native';
 import React from 'react';
 import { shallow } from 'react-native-testing-library';
-import App from '../App';
+import App from './App';
 
 // Note: test renderer must be required after react-native.
 // import renderer from 'react-test-renderer';
