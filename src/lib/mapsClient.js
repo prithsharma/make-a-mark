3784e2a2b893697378e2006696582c9c2596cf34
Prithvi Sharma <prithsharma100@gmail.com>
2019-04-18 16:59:52 +0530

    Set up geocoding client from mapbox

---
+++ src/lib/mapsClient.js
@@ -0,0 +1,10 @@
+import mapboxClient from '@mapbox/mapbox-sdk';
+import geocoding from '@mapbox/mapbox-sdk/services/geocoding';
+import Config from 'react-native-config';
+
+const baseClient = mapboxClient({ accessToken: Config.MAPBOX_ACCESS_TOKEN });
+const GeocodingClient = geocoding(baseClient);
+
+export default {
+  GeocodingClient,
+};
