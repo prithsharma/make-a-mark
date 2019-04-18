be77a9385ecbe68c8bbc60a3091a80baf5413379
Prithvi Sharma <prithsharma100@gmail.com>
2019-04-18 20:04:41 +0530

    Modify MapView base styling

--- src/components/MapView/index.js
+++ src/components/MapView/index.js
@@ -4,6 +4,7 @@ import MapboxGL from '@mapbox/react-native-mapbox-gl';
 import {
   StyleSheet,
 } from 'react-native';
+import PropTypes from 'prop-types';
 
 MapboxGL.setAccessToken(Config.MAPBOX_ACCESS_TOKEN);
 let styles;
@@ -18,10 +19,18 @@ export default class MapView extends Component {
     return (
       <MapboxGL.MapView
         style={styles.mapContainer}
+        styleURL="mapbox://styles/mapbox/dark-v9"
       />
     );
   }
 }
+
+MapView.propTypes = {
+  center: PropTypes.arrayOf(PropTypes.number),
+};
+MapView.defaultProps = {
+  center: null,
+};
 
 styles = StyleSheet.create({
   mapContainer: {
