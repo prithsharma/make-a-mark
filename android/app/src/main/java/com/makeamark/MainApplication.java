acf38bf83a3ef6b81f9f4ed5b00a73da13ce1770
Prithvi Sharma <prithsharma100@gmail.com>
2019-04-18 23:42:53 +0530

    Install react-native-snackbar

--- android/app/src/main/java/com/makeamark/MainApplication.java
+++ android/app/src/main/java/com/makeamark/MainApplication.java
@@ -3,6 +3,7 @@ package com.makeamark;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.azendoo.reactnativesnackbar.SnackbarPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(new MainReactPackage(),
+            new SnackbarPackage(),
             new VectorIconsPackage(),
             new ReactNativeConfigPackage(), new RCTMGLPackage());
     }
