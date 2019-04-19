1e3feaa7515d601c33899f9daa5a5366f6d9f456
Prithvi Sharma <prithsharma100@gmail.com>
2019-04-19 23:42:22 +0530

    yarn add redux-logger

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { configureStore } from 'redux-starter-kit';
+import { configureStore, getDefaultMiddleware } from 'redux-starter-kit';
+import logger from 'redux-logger';
 import Home from './screens/Home';
 import rootReducer from './state';
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: [...getDefaultMiddleware(), logger],
 });
 
 export default function App() {
