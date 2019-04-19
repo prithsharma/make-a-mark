504aa5f43aea049596be694dfd4ab75feda1ca7b
Prithvi Sharma <prithsharma100@gmail.com>
2019-04-19 20:19:25 +0530

    Refactor out styles to separate file

---
+++ src/screens/Home/index.styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  searchBar: {
+    position: 'absolute',
+    top: 10,
+  },
+  markersCarousel: {
+    position: 'absolute',
+    bottom: 10,
+  },
+  slideStyle: {
+    minHeight: 140,
+    justifyContent: 'center',
+  },
+});
